938d90b97270eeacfcd3db710f651ad537a95ef3
Jared Tulayan <2lyNJ.E@gmail.com>
2017-01-16 01:04:45 -0500

    Changed up Config class
    	+ Added static initialization block (Read comments)
    	+ Added setValue() and save() methods
    	~ Changed getValue() method and various javadoc

--- src/com/mercury1089/util/Config.java
+++ src/com/mercury1089/util/Config.java
@@ -1,42 +1,87 @@
 package com.mercury1089.util;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 /**
- * This class pulls a {@code ./config.properties} file for deployment configuration,
- * such as what type of environment (testing, competition) we are using the robot in.
+ * This class pulls a {@code config.properties} file for deployment configuration,
+ * such as what type of environment (testing, competition) we are using the robot in,
+ * how much fuel we have ejected over the season, and other things.
  *
  */
 public class Config {
 	private static final Properties PROP = new Properties();
 	private static final String FILE = "config.properties";
 	
+	/*
+	 * This is a static initialization block that will just initialize this class at runtime
+	 * by loading the config.properties file if it exits, or create a new one with default settings.
+	 */
+	static {
+		File f = new File(FILE);
+		
+		try {
+			if (!f.exists()) {
+				PROP.setProperty("fuel_ejected", "0");
+				
+				f.createNewFile();
+				save();
+			} else {
+				InputStream input = new FileInputStream(f);
+				
+				PROP.load(input);
+				input.close();
+			}
+		} catch (Exception e) {
+			Debug.logException(e);
+		}
+		
+	}
+	
 	/**
 	 * <pre>
-	 * public synchronized double getNumber(String key)
+	 * public synchronized String getValue(String property,
+	 *                                     String def)
 	 * </pre>
 	 * Gets a string based on the specified property.
 	 * It is up to the user to parse the string to the value they need it in.
 	 * 
 	 * @param property the property name to get a value from in the config.properties file
+	 * @param def      the default value in case the property does not exist in the config.
 	 * 
-	 * @return the value of the property if it exists, {@code null} otherwise.
+	 * @return the value of the property if it exists, {@code def} otherwise.
 	 */
-	public synchronized String getValue(String property) {
-		String val = null;
-		
-		try {
-            InputStream input = new FileInputStream(FILE);
-            PROP.load(input);
-            
-            val = PROP.getProperty(property);
-            input.close();
-        } catch (Exception e) {
-            Debug.logException(e);
-        }
-    	
-		return val;
+	public synchronized String getValue(String property, String def) {
+		return PROP.getProperty(property, def);
+	}
+	
+	/**
+	 * <pre>
+	 * public synchronized void setProperty(String key)
+	 * </pre>
+	 * Sets a specified property's value based on the given string.
+	 * It is up to the user to encode the value into a string.
+	 * 
+	 * @param property the property name to set a value of in the config.properties file
+	 */
+	public synchronized void setValue(String property, String value) {
+        PROP.setProperty(property, value);
+	}
+	
+	/**
+	 * <pre>
+	 * public void save()
+	 * </pre>
+	 * Saves the config to the {@code config.properties} file.
+	 * 
+	 * @throws IOException 
+	 */
+	public static void save() throws IOException {
+		FileWriter writer = new FileWriter(FILE);
+		PROP.store(writer, "Robot Configuration");
 	}
 }
