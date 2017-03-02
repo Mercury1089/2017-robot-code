0b74022ae58801312a63d251eb60de7e883f1eea
Srihari S <srihari_subramanian@yahoo.com>
2017-03-01 21:18:45 -0500

    Javadoc added for DriveTrain methods Hope I did this right :)

--- src/org/usfirst/frc/team1089/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team1089/robot/subsystems/DriveTrain.java
@@ -98,18 +98,42 @@ public class DriveTrain extends Subsystem implements PIDOutput{
 
 	//A: You need to turn them on to get their attention!
 	
+	/**
+     * <pre>
+	 * public void enableRobotDrive()
+	 * </pre>
+	 * Enables safety mode.
+     */
 	public void enableRobotDrive() {
 		robotDrive.setSafetyEnabled(true);
 	}
 	
+	/**
+     * <pre>
+	 * public void disableRobotDrive()
+	 * </pre>
+	 * Disables safety mode.
+     */
 	public void disableRobotDrive() {
 		robotDrive.setSafetyEnabled(false);
 	}
 	
+	/**
+     * <pre>
+	 * public void isSafetyEnabled()
+	 * </pre>
+	 * @return If SafetyMode is enabled or disabled (see enableRobotDrive() and disableRobotDrive())
+     */
 	public boolean isSafetyEnabled() {
 		return robotDrive.isSafetyEnabled();
 	}
-
+	
+	/**
+     * <pre>
+	 * public void initDefaultCommand()
+	 * </pre>
+	 * The default command for DriveTrain.
+     */
     public void initDefaultCommand() {
         // By default, drive with the joysticks
         setDefaultCommand(new DriveWithJoysticks());
@@ -132,30 +156,80 @@ public class DriveTrain extends Subsystem implements PIDOutput{
     	arcadeDrive(moveValue, rotateValue);
     }
     
+    /**
+     * <pre>
+	 * public void arcadeDrive(double moveValue, double rotateValue)
+	 * </pre>
+	 * Without joystick control, just the raw movement values for arcade drive.
+     * @param moveValue  Voltage to move forward, in percent
+     * @param rotateValue To turn in a certain direction while moving forward
+     */
     public void arcadeDrive(double moveValue, double rotateValue) {
     	robotDrive.arcadeDrive(moveValue, rotateValue);
     }
     
+    /**
+     * <pre>
+	 * public void stop()
+	 * </pre>
+	 * Stops the drive train.
+     */
     public void stop() {
     	robotDrive.stopMotor();
     }
 
+    /**
+     * <pre>
+	 * public AnalogGyro getGyro()
+	 * </pre>
+	 * Returns the gyro that is currently being used. Beware of null pointers!!!
+	 * @return The Gyro
+     */
     public AnalogGyro getGyro() { 	
     	return gyro;
     }
     
+    /**
+     * <pre>
+	 * public AHRS getNAVX()
+	 * </pre>
+	 * Returns the NavX that is currently being used. Beware of null pointers!!!
+	 * @return The NavX
+     */
     public AHRS getNAVX() {
     	return navx;
     }
 
+    /**
+     * <pre>
+	 * public double getLeftEncoder()
+	 * </pre>
+	 * Returns the current value in ticks of the encoder 
+	 * on the left drive shaft.
+	 * @return The value of the left encoder
+     */
     public double getLeftEncoder() {
     	return leftFront.getEncPosition();
     }
     
+    /**
+     * <pre>
+	 * public double getRightEncoder()
+	 * </pre>
+	 * Returns the current value in ticks of the encoder 
+	 * on the right drive shaft.
+	 * @return The value of the right encoder
+     */
     public double getRightEncoder() {
     	return -rightFront.getEncPosition();
     }
     
+    /**
+     * <pre>
+	 * public void resetEncoders()
+	 * </pre>
+	 * Resets the value of both encoders to 0.
+     */
     public void resetEncoders() {
     	leftFront.setEncPosition(0);
     	rightFront.setEncPosition(0);
@@ -170,7 +244,6 @@ public class DriveTrain extends Subsystem implements PIDOutput{
     
 	@Override
 	public void pidWrite(double output) {
-		// TODO Auto-generated method stub
 		robotDrive.tankDrive(output, output);
 	}
 	
@@ -186,43 +259,119 @@ public class DriveTrain extends Subsystem implements PIDOutput{
 		return output;
 	}
 	
+	/**
+     * <pre>
+	 * public double encoderTicksToFeet(double ticks)
+	 * </pre>
+	 * Returns a value in feet based on a certain value in ticks using
+	 * the Magnetic Encoder.
+	 * @param ticks The value in ticks
+	 * @return The value in feet
+     */
 	public double encoderTicksToFeet(double ticks) {
 		return (Math.PI * WHEEL_DIAMETER) / (MAG_ENCODER_TICKS_PER_REVOLUTION * GEAR_RATIO) * ticks;
 	}
 	
+	/**
+     * <pre>
+	 * public double feetToEncoderTicks(double feet)
+	 * </pre>
+	 * Returns a value in ticks based on a certain value in feet using
+	 * the Magnetic Encoder.
+	 * @param feet The value in feet
+	 * @return The value in ticks
+     */
 	public double feetToEncoderTicks(double feet) {
 		return (MAG_ENCODER_TICKS_PER_REVOLUTION * GEAR_RATIO) / (Math.PI * WHEEL_DIAMETER) * feet;
 	}
 	
+	/**
+     * <pre>
+	 * public double revolutionsToFeet(double revolutions)
+	 * </pre>
+	 * Returns a value in feet based on a certain value in revolutions using
+	 * the Magnetic Encoder, because the when set to position, the Magnetic Encoder
+	 * takes a value in revolutions rather than ticks.
+	 * @param revolutions The number of revolutions needed to be converted.
+	 * @return The value in feet
+     */
 	public double revolutionsToFeet(double revolutions) {
 		return revolutions * (Math.PI * WHEEL_DIAMETER);
 	}
 	
+	/**
+     * <pre>
+	 * public double feetToRevolutions(double feet)
+	 * </pre>
+	 * Returns a value in revolutions based on a certain value in feet using
+	 * the Magnetic Encoder, because the when set to position, the Magnetic Encoder
+	 * takes a value in revolutions rather than ticks.
+	 * @param feet The distance in feet needed to be converted.
+	 * @return The value in revolutions
+     */
 	public double feetToRevolutions(double feet) {
 		return feet / (Math.PI * WHEEL_DIAMETER);
 	}
 	
+	/**
+     * <pre>
+	 * public void setToVbus()
+	 * </pre>
+	 * Sets the talons to a mode which allows it to take a value between -1 and 1, a percent,
+	 * and sets it to a percent of the maximum voltage of the motor
+     */
 	public void setToVbus() {
 		leftFront.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
     	rightFront.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
 	}
 	
+	/**
+     * <pre>
+	 * public void setToVbus()
+	 * </pre>
+	 * Sets the talons to a mode which allows it to take an encoder value, and will set the encoder position to that value.
+     */
 	public void setToPosition() {
 		leftFront.changeControlMode(CANTalon.TalonControlMode.Position);
     	rightFront.changeControlMode(CANTalon.TalonControlMode.Position);
 	}
 	
+	/**
+     * <pre>
+	 * public CANTalon getRight()
+	 * </pre>
+	 * @return The right lead CANTalon
+     */
 	public CANTalon getRight() {
 		return rightFront;
 	}
+	
+	/**
+     * <pre>
+	 * public CANTalon getLeft()
+	 * </pre>
+	 * @return The left lead CANTalon
+     */
 	public CANTalon getLeft() {
 		return leftFront;
 	}
 	
+	/**
+     * <pre>
+	 * public double getRightStickVal()
+	 * </pre>
+	 * @return The X-value of the right stick, which controls turn sharpness in arcade drive.
+     */
 	public double getRightStickVal() {
 		return Robot.oi.rightStick.getX();
 	}
 	
+	/**
+     * <pre>
+	 * public double getLeftStickVal()
+	 * </pre>
+	 * @return The Y-value of the right stick, which controls throttle in arcade drive.
+     */
 	public double getLeftStickVal() {
 		return Robot.oi.leftStick.getY();
 	}
